dd548a4aa89b918cd761aa6ba1339a824e5736c7
deawon <19999eo@naver.com>
2023-11-14 11:43:28 +0900

    Step16 입출력 마무리

--- Step16_InputOutput/src/test/main/MyFrameQuiz.java
+++ Step16_InputOutput/src/test/main/MyFrameQuiz.java
@@ -20,120 +20,165 @@ import java.awt.Color;
 import java.awt.Label;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 import test.dto.MemberDto;
 
 public class MyFrameQuiz extends JFrame {
-	//필드
-	JTextField info_num;
-	JTextField info_name;
-	JTextField info_addr;
-	public MyFrameQuiz(String title) {
-		super(title);
+   //필드
+   JTextField info_num;
+   JTextField info_name;
+   JTextField info_addr;
+   List<MemberDto> infoList;
+   MemberDto dtoWrite ;
+   MemberDto dtoRead;
+   
+ 
+   
+   public MyFrameQuiz(String title) {
+      super(title);
 
-		setBounds(100, 100, 500, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//BorderLayout으로 설정
-		setLayout(new BorderLayout());
-		JTextArea ta= new JTextArea();
-		info_num= new JTextField(5);
-		info_name= new JTextField(5);
-		info_addr= new JTextField(5);
-		JLabel label_num=new JLabel("번호");
-		JLabel label_name=new JLabel("이름");
-		JLabel label_addr=new JLabel("주소");
-		
-		JButton addbtn=new JButton("회원 정보 추가");
-		
-		JPanel p1= new JPanel();
-		add(ta,BorderLayout.CENTER);
-		p1.add(label_num);
-		p1.add(info_num);
-		p1.add(label_name);
-		p1.add(info_name);
-		p1.add(label_addr);
-		p1.add(info_addr);
-		p1.add(addbtn);
-		this.add(p1, BorderLayout.SOUTH);
-		p1.setBackground(Color.YELLOW);
-		
-		addbtn.addActionListener((e)->{
-			String info_Num= info_num.getText();
-			String info_Name= info_name.getText();
-			String info_Addr= info_addr.getText();
-			//필요한 객체의 참조값을 담을 지역 변수를 미리 만들기
-			FileOutputStream fos=null;
-			ObjectOutputStream oos=null;
-			
-			FileInputStream fis =null;
-			ObjectInputStream ois=null;
-			
-			
-			MemberDto dtoWrite = new MemberDto(Integer.parseInt(info_Num),info_Name,info_Addr);
-			
-			try {
-				//쓰기
-				fos= new FileOutputStream("c:/acorn202310/MyFolder/members.dat");
-				oos= new ObjectOutputStream(fos);
-				oos.writeObject(dtoWrite);
-				
-				//읽어드리기
-				fis= new FileInputStream("c:/acorn202310/MyFolder/members.dat");
-				ois=new ObjectInputStream(fis);
-				
-				//읽어낸 Object를 원래 type인 MemberDto type으로 캐스팅 한다
-				MemberDto dtoRead=(MemberDto)ois.readObject();
-				String info=String.format("%d %s %s",dtoRead.getNum(),dtoRead.getName(),dtoRead.getAddr() );
-				System.out.println(info);
-				
-				
-			}catch(Exception e1) {
-				e1.printStackTrace();
-			}finally {
-				try {
-					if(oos!=null)oos.close();
-					if(fos!=null)fos.close();
-					
-					if(fis!=null) fis.close();
-					if(ois!=null) ois.close();
-				}catch(Exception e2) {}
-			}
-			
-			
-			
-			
-			
-		});
-		
-	}
+      setBounds(100, 100, 700, 500);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //BorderLayout으로 설정
+      setLayout(new BorderLayout());
+      
+      //표 형식으로 출력할 JTable
+      JTable table = new JTable();
+      //테이블의 칼럼명을 미리 배열로 준비한다.
+      String[] colNames= {"번호","이름", "주소"};
+      DefaultTableModel model= new DefaultTableModel(colNames,0);
+      //모델을 테이블에 연결한다
+      table.setModel(model);
+      
+      //테이블을 스크롤 패널로 감싸고
+      JScrollPane scroll=new JScrollPane(table);
+      
+      //스크롤 패널을 프레임의 가운데에 배치하기
+      add(scroll,BorderLayout.CENTER);
+      
+      
+      info_num= new JTextField(5);
+      info_name= new JTextField(5);
+      info_addr= new JTextField(5);
+      JLabel label_num=new JLabel("번호");
+      JLabel label_name=new JLabel("이름");
+      JLabel label_addr=new JLabel("주소");
+      
+      JButton addbtn=new JButton("회원 정보 추가");
+      
+      JPanel p1= new JPanel();
+      
+      p1.add(label_num);
+      p1.add(info_num);
+      p1.add(label_name);
+      p1.add(info_name);
+      p1.add(label_addr);
+      p1.add(info_addr);
+      p1.add(addbtn);
+      this.add(p1, BorderLayout.SOUTH);
+      p1.setBackground(Color.YELLOW);
+   
+      //읽어드리기
+    
+      
+     
+      
+      infoList= new ArrayList<MemberDto>();
+      addbtn.addActionListener((e)->{
+         String info_Num= info_num.getText();
+         String info_Name= info_name.getText();
+         String info_Addr= info_addr.getText();
+         //필요한 객체의 참조값을 담을 지역 변수를 미리 만들기
+         FileOutputStream fos=null;
+         ObjectOutputStream oos=null;
+         
+         FileInputStream fis=null;
+         ObjectInputStream ois=null;
+         
+         
+         
+         dtoWrite = new MemberDto(Integer.parseInt(info_Num),info_Name,info_Addr);
+         infoList.add(dtoWrite);
+         
+         try {
+            
+            //쓰기
+            fos= new FileOutputStream("c:/acorn202310/MyFolder/my_members.dat");
+            oos= new ObjectOutputStream(fos);
+            
+            //읽어드리기
+            fis= new FileInputStream("c:/acorn202310/MyFolder/my_members.dat");
+            ois=new ObjectInputStream(fis);
+            
+         
+            
+            for(MemberDto tmp:infoList) {
+               oos.writeObject(tmp);
 
-	
-	
-	public static void main(String[] args) {
-		MyFrameQuiz f=new MyFrameQuiz("회원정보 추가 목록");
-		f.setVisible(true);
-		
-		List<MemberDto> infoList= new ArrayList<>();
-		
-		
-		
-		
-		
-		
-	}
+               dtoRead=(MemberDto)ois.readObject();
+               String info=String.format("%d %s %s",dtoRead.getNum(),dtoRead.getName(),dtoRead.getAddr() );
+               System.out.println(info);
+               
+               
+            }
+            
+            Vector<Object> row= new Vector<Object>();
+            
+            row.add(dtoRead.getNum());
+            row.add(dtoRead.getName());
+            row.add(dtoRead.getAddr());
+            model.addRow(row);
+          
+            
+         }catch(Exception e1) {
+            e1.printStackTrace();
+            
+         }finally {
+            try {
+               if(oos!=null)oos.close();
+               if(fos!=null)fos.close();
+               
+               if(fis!=null) fis.close();
+               if(ois!=null) ois.close();
+            }catch(Exception e2) {}
+         }
+         
+      });
+      
+   }
+
+   
+   
+   public static void main(String[] args) {
+      MyFrameQuiz f=new MyFrameQuiz("회원정보 추가 목록");
+      f.setVisible(true);
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+   }
 }
-
 
 
 
