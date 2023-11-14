17ccf559a6ae3427a9ac3d6f77ae230c3d7a5ba2
deawon <19999eo@naver.com>
2023-11-14 18:26:42 +0900

    jdbc_mainClass01 수정

--- Step17_JDBC/src/test/main/MainClass01.java
+++ Step17_JDBC/src/test/main/MainClass01.java
@@ -6,8 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 /*
- * 프로젝트에 마우스 우클릭 =>Build Path=>Configure Build Path
- * =>classpath 선택=>
+ *  JDBC ( Java DataBase Connectivity )
+ *  
+ *  DataBase 에 연결해서 SELECT, INSERT, UPDATE, DELETE 작업하기
+ *  
+ *  Oracle 에 연결하기 위해서는 드라이버 클래스가 들어있는 ojdbc6.jar 파일을
+ *  사용할수 있도록 설정해야 한다.
+ *  
+ *  프로젝트에 마우스 우클릭 => Build Path => Configure Build Path => Librarys 텝 선택
+ *  => classpath 선택 => 우측 Add External jar 버튼을 누른후 => ojdbc6.jar 파일을 찾은다음 => Apply 
  */
 public class MainClass01 {
 	public static void main(String[] args) {
--- Step17_JDBC/src/test/main/MainClass09.java
+++ Step17_JDBC/src/test/main/MainClass09.java
@@ -1,14 +1,39 @@
 package test.main;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import test.Util.DBConnect;
 import test.dto.MemberDto;
 
 public class MainClass09 {
 	//전체 회원의 목록을 리턴하는 메소드
 	public static List<MemberDto> getList(){
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		try {
+			//미리 준비한 util 클래스를 이용해서 Connection 객체 얻어오기 
+			conn=new DBConnect().getConn();
+			//실행할 미완성의 sql 문
+			String sql="";
+			//미완성의 sql 문을 전달하면서 PreparedStatement 객체의 참조값 얻어내기
+			pstmt=conn.prepareStatement(sql);
+			//PreparedStatement 객체의 메소드를 이용해서 미완성인 sql 문을 완성시키기(? 에 값 바인딩하기)
+			
+			//sql 문 실행하기
+			pstmt.executeUpdate();
+			System.out.println("회원 정보를 가져왔습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
 		
-		return null;
+		
 	}
 	
 	//run했을 때 실행의 흐름이 시작되는 main 메소드
